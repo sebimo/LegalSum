33e0772b236adb5af7f2ba003f10d41bf73da403
Sebastian Moser <sebastian.moser@onlinehome.de>
2021-01-26 23:12:04 +0100

    Added training logging support for tensorboard + own database

---
+++ src/model_logging/logger.py
@@ -0,0 +1,141 @@
+import sqlite3
+from enum import Enum
+from datetime import datetime
+from pathlib import Path
+import numpy as np
+
+from torch.utils.tensorboard import SummaryWriter
+
+class Type(Enum):
+    TEXT = 1
+    INT = 2
+    FLOAT = 3
+
+class Logger:
+
+    def __init__(self, database: str="experiment.db"):
+        print("Starting experiment database:", database)
+        self.conn = sqlite3.connect(database)
+        self.c = self.conn.cursor()
+        self.__setup_experiment_db__()
+        self.__setup_epoch_db__()
+        self.experiment = None
+        self.exp_num = None
+        self.epoch = 0
+
+    def add_parameter(self, name: str, type: Type):
+        type_str = "TEXT" if type == Type.TEXT else ("INTEGER" if type == Type.INT else "REAL")
+        request = "ALTER TABLE experiment ADD COLUMN "+name+" "+type_str+" DEFAULT NULL"
+        try:
+            self.c.execute(request)
+            print("Add parameter: "+name)
+        except sqlite3.OperationalError:
+            pass
+
+    def add_perf_metric(self, name: str, type: Type):
+        type_str = "TEXT" if type == Type.TEXT else ("INTEGER" if type == Type.INT else "REAL")
+        request = "ALTER TABLE epoch ADD COLUMN "+name+" "+type_str+" DEFAULT NULL"
+        try:
+            self.c.execute(request)
+            print("Add performance metric: "+name)
+        except sqlite3.OperationalError:
+            pass
+
+    def start_experiment(self, parameters: dict={}):
+        time = datetime.now()
+        self.experiment = time.strftime("%d_%m_%Y %H%M%S")
+        columns = [i[1] for i in self.c.execute("PRAGMA table_info(experiment)")]
+        request = "INSERT INTO experiment ("
+        for key in parameters:
+            if key not in columns:
+                raise sqlite3.OperationalError("Unknown column: "+key)
+            request += str(key)+","
+        request += "date) VALUES ("
+        for key in parameters:
+            if isinstance(parameters[key], str):
+                request += "\""+str(parameters[key])+"\","
+            else:
+                request += str(parameters[key])+","
+        request += "\""+str(self.experiment)+"\");"
+        print(request)
+        self.c.execute(request)
+        self.conn.commit()
+
+        comment = []
+        for key in parameters:
+            comment.append(str(key))
+            comment.append(str(parameters[key]))
+        comment = "_".join(comment)
+        self.writer = SummaryWriter(log_dir=Path("runs")/(self.experiment+"_"+comment))
+
+        self.c.execute("SELECT rowid FROM experiment WHERE date=\""+self.experiment+"\";")
+        self.exp_num = self.c.fetchone()[0]
+        self.epoch = 0
+
+    def log_epoch(self, metrics: dict):
+        if self.exp_num is None or self.experiment is None:
+            raise sqlite3.OperationalError("Experiment not started.")
+        self.epoch += 1
+        columns = [i[1] for i in self.c.execute("PRAGMA table_info(epoch)")]
+        request = "INSERT INTO epoch ("
+        for key in metrics:
+            if key not in columns:
+                raise sqlite3.OperationalError("Unknown column: "+key)
+            request += str(key)+","
+        request += "experiment,epoch) VALUES ("
+        for key in metrics:
+            request += str(metrics[key])+","
+        request += str(self.exp_num)+","+str(self.epoch)+");"
+        for key in metrics:
+            self.writer.add_scalar(key, metrics[key], self.epoch)
+        self.c.execute(request)
+        self.conn.commit()
+
+    def exp_info(self):
+        if self.exp_num is None or self.experiment is None:
+            raise KeyError("No experiment selected.")
+        self.c.execute("SELECT * FROM experiment WHERE date=\""+str(self.experiment)+"\";")
+        params = self.c.fetchone()
+        self.c.execute("SELECT * FROM epoch WHERE experiment="+str(self.exp_num)+";")
+        epochs = self.c.fetchall()
+        return {"parameter": params, "epochs": epochs}
+
+    def log_model(self, model):
+        self.writer.add_graph(model)
+
+    def __setup_experiment_db__(self):
+        self.c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='experiment'")
+        if not self.c.fetchone()[0] == 1:
+            print("Creating table: experiment")
+            self.c.execute("CREATE TABLE experiment (date TEXT NOT NULL PRIMARY KEY)")
+            self.conn.commit()
+
+    def __setup_epoch_db__(self):
+        self.c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='epoch'")
+        if not self.c.fetchone()[0] == 1:
+            print("Creating table: epoch")
+            self.c.execute("CREATE TABLE epoch (experiment INTEGER NOT NULL, epoch INTEGER NOT NULL)")
+            self.conn.commit()
+
+    def __del__(self):
+        print("Closing connection to experiment database.")
+        self.conn.commit()
+        self.conn.close()
+
+
+if __name__ == "__main__":
+    logger = Logger(database="test.db")
+    logger.add_parameter("test_int", Type.INT)
+    logger.add_perf_metric("acc", Type.FLOAT)
+    try:
+        logger.start_experiment(parameters={"test_int": 1, "hello": "world"})
+    except sqlite3.OperationalError:
+        print("Handled unknown column")
+    try:
+        logger.log_epoch(metrics={})
+    except sqlite3.OperationalError:
+        print("Unstarted experiment.")
+    logger.start_experiment(parameters={"test_int": 1})
+    for i in range(50):
+        logger.log_epoch({"acc": 50+i*0.5 + np.random.random()*10})
+    print(logger.exp_info())
