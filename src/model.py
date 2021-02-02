3f25f8de633fc3bd622d6ed35dd7c16457dac3a2
Sebastian Moser <sebastian.moser@onlinehome.de>
2021-02-02 13:46:04 +0100

    Added first encoder implementations + testing (Hierarchical + RNN)

--- src/model.py
+++ src/model.py
@@ -1,2 +1,163 @@
 # Contains all the models and its components for the summarization task
 # We might want to compile the models to get faster training, is that possible?
+# TODO Attention-layer for Encoding
+
+import torch
+import torch.nn as nn
+
+class HierarchicalEncoder(nn.Module):
+
+    def __init__(self,
+                 embedding_size: int = 200,
+                 n_tokens: int = 50000,
+                 activation: nn.Module = nn.ReLU(),
+                 dropout: float = 0.2,
+                 embedding_layer: nn.Module=nn.Embedding):
+        super().__init__()
+        self.embedding_size = embedding_size
+        self.dropout = dropout
+        self._activation = activation
+        
+        # We might want to replace this with something different
+        self._embedding = embedding_layer(n_tokens, embedding_size)
+        self._emb_layers = nn.Sequential(
+            nn.Dropout(p=self.dropout),
+            nn.Linear(self.embedding_size, self.embedding_size),
+            self._activation,
+        )
+        self._classification = nn.Sequential(
+            nn.Linear(self.embedding_size, 1),
+            nn.Sigmoid()
+        )
+
+        self._token_query = nn.Linear(self.embedding_size, 1, bias=False)
+        self._sent_layers = nn.Sequential(
+            nn.Dropout(p=self.dropout),
+            nn.Linear(self.embedding_size, self.embedding_size),
+            self._activation,
+        )
+        self._sentence_query = nn.Linear(self.embedding_size, 1, bias=False)
+        self._attention_softmax = nn.Softmax(dim=-1)
+    
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = self._embedding(X)
+        X = self._emb_layers(X)
+
+        X = self.__get_token_attention__(X)
+        X = self.__get_sentence_attention__(X)
+
+        X = self._activation(X)
+        X = self._classification(X)
+        return X
+
+    def classify(self, E: torch.Tensor) -> torch.Tensor:
+        y = self._classification(E)
+        return y
+
+    def __get_token_weights__(self, X: torch.Tensor) -> torch.Tensor:
+        """ Returns the attention weights for calculating the sentence encoding 
+        Arguments:
+            X : torch.Tensor(batch_size, num_sentences = 200, num_tokens = 100, embedding_size)
+        Returns:
+            A : torch.Tensor(batch_size, num_sentences = 200, num_tokens = 100, 1) -- weight for each token indicating its importance
+        """
+        weights = self._token_query(X)
+        # We need to normalize them:
+        weights = self._attention_softmax(weights)
+        return weights
+
+    def __get_token_attention__(self, X: torch.Tensor) -> torch.Tensor:
+        """ Calculates the weighted sum over the token embeddings 
+        Arguments:
+            X : torch.Tensor(batch_size, num_sentences = 200, num_tokens = 100, embedding_size)
+        Returns:
+            torch.Tensor(batch_size, num_sentences = 200, embedding_size)
+        """
+        # Get the token weights + apply them to all token embeddings
+        weights = self.__get_token_weights__(X)
+        X = torch.mul(X, weights)
+        # Reduce them by summing of all weighted token embeddings (-2 as we want to keep the last embedding dimension)
+        X = torch.sum(X, dim=-2)
+        return X
+
+    def __get_sentence_weights__(self, X: torch.Tensor) -> torch.Tensor:
+        """ Returns the attention weights for calculating the sentence encoding 
+        Arguments:
+            X : torch.Tensor(batch_size, num_sentences = 200, embedding_size)
+        Returns:
+            A : torch.Tensor(batch_size, num_sentences = 200, 1) -- weight for each sentence indicating its importance
+        """
+        weights = self._sentence_query(X)
+        # We need to normalize them
+        weights = self._attention_softmax(weights)
+        return weights
+
+    def __get_sentence_attention__(self, X: torch.Tensor) -> torch.Tensor:
+        """ Calculates the weighted sum over the sentence embeddings
+        Arguments:
+            X : torch.Tensor(batch_size, num_sentences = 200, embedding_size)
+        Returns:
+            torch.Tensor(batch_size, embedding_size)
+        """
+        # Get the sentence weights + apply them to all sentence embeddings
+        weights = self.__get_sentence_weights__(X)
+        X = torch.mul(X, weights)
+        # Reduce the sentences by summing over all weighted sentence embeddings
+        X = torch.sum(X, dim=-2)
+        return X
+
+class RNNEncoder(nn.Module):
+
+    def __init__(self,
+                 embedding_size: int = 200,
+                 n_tokens: int = 50000,
+                 layers = 2,
+                 bidirectional = True,
+                 activation: nn.Module = nn.ReLU(),
+                 dropout: float = 0.2,
+                 embedding_layer: nn.Module=nn.Embedding):
+        super().__init__()
+        self.embedding_size = embedding_size
+        self.dropout = dropout
+        self._activation = activation
+        
+        # We might want to replace this embedding layer with something different
+        self._embedding = embedding_layer(n_tokens, embedding_size)
+        self.layers = layers
+        self.bidirectional = bidirectional
+
+        self.gru1 = nn.GRU(self.embedding_size, self.embedding_size, num_layers=layers, bidirectional=self.bidirectional)
+        self.gru_size = self.embedding_size*self.layers*(2 if self.bidirectional else 1)
+        self.reduction1 = nn.Linear(self.gru_size, self.embedding_size)
+        self.reduction2 = nn.Linear(self.embedding_size, self.embedding_size)
+
+        self._classification = nn.Sequential(
+            nn.Linear(self.embedding_size, 1),
+        )
+        self.sig = nn.Sigmoid()
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = self._embedding(X)
+
+        initial_shape = X.shape
+        #X = X.reshape((-1, initial_shape[-2], self.embedding_size))
+
+        # RNN over the tokens in a sentence
+        X, _ = self.gru1(X)
+        X = X.reshape(initial_shape[0], initial_shape[1], self.gru_size)
+        # Max over -2 as, we want to keep the embedding dimensionality
+        X = torch.mean(X, dim=-2, keepdim=True)
+        X = torch.squeeze(X, dim=-2)
+
+        X = self.reduction1(X)
+        X = self._activation(X)
+
+        X = self.reduction2(X)
+        X = self._activation(X)
+
+        X = self._classification(X)
+        return X
+
+    def classify(self, E: torch.Tensor) -> torch.Tensor:
+        y = self.sig(E)
+        return y
--- src/testing/model_test.py
+++ src/testing/model_test.py
@@ -1,11 +1,98 @@
 import pytest
+import torchtest
 import torch
 
-class TestModel:
+from ..model import HierarchicalEncoder, RNNEncoder
 
-    def setup(self):
-        pass
+class TestHierarchicalEncoder:
 
-    def test_basics(self):
+    def setup(self, max_sent=200, max_token=100, embedding_size=200, n_tokens=500):
+        self.max_sent = max_sent
+        self.max_token = max_token
+        self.embedding_size = embedding_size
+        self.n_tokens = n_tokens
+        self.model = HierarchicalEncoder(embedding_size=self.embedding_size, n_tokens=self.n_tokens)
+
+    def setup_batch(self):
         self.setup()
+        batch = torch.rand((self.max_sent, self.max_token, self.embedding_size))
+        return batch
+
+    def setup_indice_batch(self):
+        self.setup(max_sent=10, max_token=10, embedding_size=200, n_tokens=100)
+        batch = torch.zeros((self.max_sent, self.max_token), dtype=torch.long)
+        for s in range(self.max_sent):
+            for t in range(self.max_token):
+                batch[s][t] = torch.randint(low=0, high=self.n_tokens, size=(1,))
+
+        return batch
+
+    def test_token_weights_shape(self):
+        batch = self.setup_batch()
+        output = self.model.__get_token_weights__(batch)
+        assert len(output.shape) == 3
+        o_s, o_t, o_e = output.shape
+        assert o_s == self.max_sent
+        assert o_t == self.max_token
+        assert o_e == 1
+
+    def test_token_weights_softmax(self):
+        batch = self.setup_batch()
+        output = self.model.__get_token_weights__(batch)
+        weight_sum = torch.sum(output, dim=-1)
+        for i in range(self.max_token):
+            assert weight_sum[0][i] == 1.0
+
+    def test_sentence_weights_shape(self):
+        self.setup()
+        batch = torch.rand((self.max_sent, self.embedding_size))
+        output = self.model.__get_sentence_weights__(batch)
+        assert len(output.shape) == 2
+        o_s, o_e = output.shape
+        assert o_s == self.max_sent
+        assert o_e == 1
+
+    def test_sentence_weights_softmax(self):
+        self.setup()
+        batch = torch.rand((self.max_sent, self.embedding_size))
+        output = self.model.__get_sentence_weights__(batch)
+        weight_sum = torch.sum(output, dim=-1)
+        print(weight_sum.shape)
+        for i in range(self.max_sent):
+            assert weight_sum[i] == 1.0
+
+    def test_model(self):
+        batch = self.setup_indice_batch()
+        pred = self.model(batch)
+        # We just want to check, if there is any error in the model (dimensions, ...)
+        assert True == True
+
+
+class TestRNNEncoder:
+
+    def setup(self, max_sent=200, max_token=100, embedding_size=200, n_tokens=500):
+        self.max_sent = max_sent
+        self.max_token = max_token
+        self.embedding_size = embedding_size
+        self.n_tokens = n_tokens
+        self.model = RNNEncoder(embedding_size=self.embedding_size, n_tokens=self.n_tokens, layers=1)
+
+    def setup_batch(self):
+        self.setup()
+        batch = torch.rand((self.max_sent, self.max_token, self.embedding_size))
+        return batch
+
+    def setup_indice_batch(self):
+        self.setup(max_sent=10, max_token=10, embedding_size=200, n_tokens=100)
+        batch = torch.zeros((self.max_sent, self.max_token), dtype=torch.long)
+        for s in range(self.max_sent):
+            for t in range(self.max_token):
+                batch[s][t] = torch.randint(low=0, high=self.n_tokens, size=(1,))
+
+        return batch
+
+    def test_forward(self):
+        batch = self.setup_indice_batch()
+        pred = self.model(batch)
+        # Just checks for any bugs in dimensions etc.
         assert True
