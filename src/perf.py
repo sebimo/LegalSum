aac4739538bf7262d1edbd3b95e0e07eb1e1741c
Sebastian Moser <sebastian.moser@onlinehome.de>
2021-02-20 10:40:56 +0100

    Moved preprocessing performance testing script

--- src/perf.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-from preprocessing import load_verdict
-
-PATH = Path("..")/"data"/"dataset"
-
-for file in os.listdir(PATH)[3000:5000]:
-    verdict = load_verdict(PATH/file)
