e39031a3e43c807f18ce4d287ef7982b62cb1e7f
Sebastian Moser <sebastian.moser@onlinehome.de>
2021-02-22 19:12:11 +0100

    Expanded training routine

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def start():
     tok = Tokenizer(TOKENIZER_PATH, normalize=True, mapping=embedding.get_word_mapping())
     # TODO reduced dataset
     # TODO REMOVE ALL FILES WITH EMPTY REASONING/FACTS OR NO TARGET
-    trainset = ExtractiveDataset(get_train_files()[:10], tok)
-    valset = ExtractiveDataset(get_val_files()[:10], tok)
-    for _ in range(1):
+    trainset = ExtractiveDataset(get_train_files(), tok)
+    valset = ExtractiveDataset(get_val_files(), tok)
+    for _ in range(2):
         model = (
             HierarchicalEncoder(embedding_size=embedding_size, 
                                 n_tokens=tok.get_num_tokens(),
@@ -46,14 +46,15 @@ def start():
             "model": model[1],
             "lr": lr,
             "abstractive": 1 if ABSTRACTIVE else 0,
-            "embedding": embedding.get_name()
+            "embedding": embedding.get_name(), 
+            "attention": "DOT"
         }
         logger.start_experiment(logger_params)
 
         trainer = Trainer(model[0], trainset, valset, logger, ABSTRACTIVE)
-        trainer.train(LossType.BCE, epochs=200, lr=lr)
+        trainer.train(LossType.BCE, epochs=20, lr=lr)
 
-def random_log_lr(lr_range: Tuple[float, float]=(1e-2, 1e-6)) -> float:
+def random_log_lr(lr_range: Tuple[float, float]=(1e-1, 1e-3)) -> float:
     """ Will draw a random learning rate on a logarithmic scale, i.e. drawing the  lr from (1e-5, 1e-4) is as likely as from (1e-2,1e-1) """
     # convert bounds to logarithmic scale
     log_lower = np.log(lr_range[0])
