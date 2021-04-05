caaae40f3bcc6fde34fb6d20bd98bdcf8fe28151
Sebastian Moser <sebastian.moser@onlinehome.de>
2021-04-05 17:24:47 +0200

    Added oracle evalution

---
+++ oracle.py
@@ -0,0 +1,59 @@
+from tqdm import tqdm
+from pathlib import Path
+import os
+
+from src.dataloading import get_ext_target_indices
+from src.preprocessing import Tokenizer
+from src.evaluation import evaluate
+
+tok = Tokenizer(Path("model"))
+DATA_PATH = Path("data/dataset")
+DB_PATH = Path("data/databases/extractive.db")
+
+scores = []
+"""for file in tqdm(os.listdir(DATA_PATH)):
+    verdict = tok.tokenize_verdict_without_id(DATA_PATH/file)
+    f_inds, r_inds = get_ext_target_indices(DATA_PATH/file, DB_PATH, tok)
+
+    sentences = []
+    for ind in f_inds:
+        sentences += verdict["facts"][ind]
+    for ind in r_inds:
+        sentences += verdict["reasoning"][ind]
+    
+    gp = []
+    for sent in verdict["guiding_principle"]:
+        gp += sent
+
+    score = evaluate([gp], [sentences])[0]
+    scores.append((score["rouge-1"]["f"], score["rouge-2"]["f"]))
+
+r1 = sum(map(lambda x: x[0], scores))/len(scores)
+r2 = sum(map(lambda x: x[1], scores))/len(scores)
+print("One-to-One R1:", r1, "; R2:", r2)"""
+
+DB_PATH = Path("data/databases/extractive_greedy.db")
+scores = []
+for file in tqdm(os.listdir(DATA_PATH)):
+    verdict = tok.tokenize_verdict_without_id(DATA_PATH/file)
+    f_inds, r_inds = get_ext_target_indices(DATA_PATH/file, DB_PATH, tok)
+
+    sentences = []
+    for ind in f_inds:
+        sentences += verdict["facts"][ind]
+    for ind in r_inds:
+        sentences += verdict["reasoning"][ind]
+    
+    gp = []
+    for sent in verdict["guiding_principle"]:
+        gp += sent
+
+    if len(sentences) == 0:
+        sentences = ["<unk>"]
+
+    score = evaluate([gp], [sentences])[0]
+    scores.append((score["rouge-1"]["f"], score["rouge-2"]["f"]))
+
+r1 = sum(map(lambda x: x[0], scores))/len(scores)
+r2 = sum(map(lambda x: x[1], scores))/len(scores)
+print("Greedy R1:", r1, "; R2:", r2)
