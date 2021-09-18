be1cc9c1af882b02b363da87eed38e5f16bc6322
Sebastian Moser <sebastian.moser@in.tum.de>
2021-09-18 13:32:30 +0200

    Removed test script

--- data/test.py
+++
@@ -1,9 +0,0 @@
-import io
-import os
-import json
-from pathlib import Path
-from tqdm import tqdm
-
-with io.open("processed_data_nrw/10_S_193_09urteil20100520.json", "r", encoding="utf-8") as f:
-    dic = json.load(f)
-    print(dic["guiding_principle"][0])
